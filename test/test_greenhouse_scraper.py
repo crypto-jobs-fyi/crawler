48fd80045d1cf99dfdfe3ccc25b2882c91f841cb
yury-dubinin <yury.dubinin@gmail.com>
2023-05-27 18:38:39 +0200

    added trmlabs

--- test/test_greenhouse_scraper.py
+++ test/test_greenhouse_scraper.py
@@ -7,6 +7,8 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
 company_list = [
+    CompanyItem('trmlabs', 'https://www.trmlabs.com/careers-list', ScrapeGreenhouse,
+                'https://www.trmlabs.com', 'Web3 Information'),
     CompanyItem('foundrydigital', 'https://boards.greenhouse.io/foundrydigital', ScrapeGreenhouse,
                 'https://foundrydigital.com', 'Web3'),
     CompanyItem('o1labs', 'https://boards.greenhouse.io/o1labs', ScrapeGreenhouse, 'https://o1labs.org',
