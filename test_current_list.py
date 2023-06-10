9beab68d9629a1f373ac1c5c0324f9f9153f6b9d
yury-dubinin <yury.dubinin@gmail.com>
2023-06-10 21:06:47 +0200

    up 10-6

--- test_current_list.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('current.json', 'r') as f:
-    jobs_data = json.load(f)
-    print(f'Number of records: {len(jobs_data)}')
-
-for i in jobs_data:
-    if jobs_data[i] == 0:
-        print(i)
