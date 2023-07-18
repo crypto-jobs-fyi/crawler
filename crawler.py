bf4893cbe9389947d5dff47e60fe3eab9eb7ff22
yury-dubinin <yury.dubinin@gmail.com>
2023-07-18 18:51:03 +0200

    up 18-08

--- crawler.py
+++ crawler.py
@@ -1,11 +1,10 @@
-import json
 import time
 from datetime import datetime
 
 from selenium import webdriver
 
-from src.company_list import get_company, write_companies
 from src.company_list import get_company_list
+from src.company_list import write_companies
 
 company_list = get_company_list()
 print(f'[CRAWLER] Number of companies: {len(company_list)}')
@@ -22,20 +21,6 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-extensions')
 driver = webdriver.Chrome(options=chrome_options)
 
-# count open positions
-total_number_of_jobs: int = 0
-current_jobs = {}
-
-
-def write_numbers():
-    global total_number_of_jobs
-    print(f'[CRAWLER] In Total {total_number_of_jobs} of open positions on {datetime.date(datetime.now())}')
-    global current_jobs
-    current_jobs["Total Jobs"] = total_number_of_jobs
-    with open("current.json", "w") as file:
-        json.dump(current_jobs, file, indent=4)
-
-
 n = 1
 
 for company in company_list:
@@ -48,31 +33,3 @@ for company in company_list:
     print('[CRAWLER] Execution time:', (time.time() - st), 'seconds')
 
 driver.close()
-
-write_numbers()
-
-
-def write_history(current_jobs_json: dict):
-    now = f'{datetime.date(datetime.now())}'
-    with open('history.json', 'r') as f:
-        jobs_data_json = json.load(f)
-    for job in current_jobs_json:
-        it = jobs_data_json.get(job, {})
-        it[now] = current_jobs_json[job]
-        jobs_data_json[job] = it
-    with open('history.json', 'w') as file:
-        json.dump(jobs_data_json, file, indent=4)
-
-
-with open('current.json') as json_file:
-    data = json.load(json_file)
-    write_history(dict(data))
-
-print('--- No jobs found for following companies: ---')
-with open('current.json', 'r') as current_file:
-    current_jobs_data = json.load(current_file)
-
-for i in current_jobs_data:
-    if current_jobs_data[i] == 0:
-        print(f'Company: {i} Jobs board: {get_company(i).jobs_url}')
-print('^^^ No jobs found for the above companies: ^^^')
--- write_jobs_age.py
+++ write_jobs_age.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 with open('jobs.json', 'r') as f:
     jobs_json = json.load(f)
@@ -9,14 +10,46 @@ with open('jobs_age.json', 'r') as age_file:
     age_data = json.load(age_file)
     print(f'Number of jobs ages stored: {len(age_data)}')
 
+current_jobs = {"Total Jobs": len(jobs_data)}
+for j in jobs_data:
+    company_name = j.get('company')
+    current_jobs[company_name] = current_jobs.get(company_name, 0) + 1
+
+with open("current.json", "w") as file:
+    json.dump(current_jobs, file, indent=4)
+
 for job in jobs_data:
     link = job.get('link')
     num = age_data.get(link, 0)
     if num != 0:
-        age_data[link] = num+1
+        age_data[link] = num + 1
     else:
         age_data[link] = 1
 
 print(f'Number of jobs processed: {len(age_data)}')
 with open('jobs_age.json', 'w') as file:
     json.dump(age_data, file, indent=4)
+
+
+def write_history(current_jobs_json: dict):
+    now = f'{datetime.date(datetime.now())}'
+    with open('history.json', 'r') as f:
+        jobs_data_json = json.load(f)
+    for c_job in current_jobs_json:
+        it = jobs_data_json.get(c_job, {})
+        it[now] = current_jobs_json[c_job]
+        jobs_data_json[c_job] = it
+    with open('history.json', 'w') as history_file:
+        json.dump(jobs_data_json, history_file, indent=4)
+
+
+print('--- No jobs found for following companies: ---')
+with open('current.json', 'r') as current_json_file:
+    c_data = json.load(current_json_file)
+    write_history(dict(c_data))
+
+for i in c_data:
+    print(i)
+    if c_data[i] == 0:
+        print(f'Company: {i} Jobs board: ')
+print('^^^ No jobs found for the above companies: ^^^')
