cc551b64865e62fc6ff634254386843cbe94259b
yury-dubinin <yury.dubinin@gmail.com>
2023-10-17 15:16:25 +0200

    update osl

--- test/test_any_scraper.py
+++ test/test_any_scraper.py
@@ -6,6 +6,7 @@ from src.scrape_ashbyhq import ScrapeAshbyhq
 from src.scrape_greenhouse import ScrapeGreenhouse
 from src.scrape_workable import ScrapeWorkable
 from src.scrape_lever import ScrapeLever
+from src.scrape_bamboohr import ScrapeBamboohr
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -20,6 +21,7 @@ company_list = [
                 'https://cryptio.co', 'Web3 Back Office'),
     CompanyItem('dydx', 'https://boards.greenhouse.io/dydx', ScrapeGreenhouse,
                 'https://dydx.exchange', 'Web3 Exchange'),
+    CompanyItem('osl', 'https://bcgroup.bamboohr.com/careers', ScrapeBamboohr, '', '')
 ]
 
 for company in company_list:
@@ -31,8 +33,8 @@ driver.close()
 
 
 class TestScraper(unittest.TestCase):
-    def test_upper(self):
-        self.assertGreater(len(jobs_data), 1)
+    def test_jobs_data(self):
+        self.assertGreater(len(jobs_data), 1, f"Empty list for {company.company_name}")
 
 
 if __name__ == '__main__':
