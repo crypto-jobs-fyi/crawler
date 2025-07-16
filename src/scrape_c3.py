75a03ac91ae1b3dbca76b2d3671f1042d6d17868
Yury Dubinin <yury.dubinin@gmail.com>
2025-07-16 12:02:19 +0200

    feat: add ScrapeC3 class and test for scraping jobs from c3.ai

---
+++ src/scrape_c3.py
@@ -0,0 +1,33 @@
+from selenium.webdriver.common.by import By
+from src.scrape_it import ScrapeIt
+
+
+def to_records(driver, company) -> []:
+    group_elements = driver.find_elements(By.CSS_SELECTOR, 'div[data-dept_id] > a[href*="c3"]')
+    result = []
+    print(f'[{company}] Found {len(group_elements)} jobs. Scraping jobs...')
+    for elem in group_elements:
+        job_url = elem.get_attribute('href')
+        title = elem.find_element(By.CSS_SELECTOR, 'h4[class="title"]').text
+        job = {
+            "company": company,
+            "title": title,
+            "location": elem.find_element(By.CSS_SELECTOR, 'h6[class="location"]').text,
+            "link": f"<a href='{job_url}' target='_blank' >Apply</a>"
+        }
+        result.append(job)
+    return result
+
+
+class ScrapeC3(ScrapeIt):
+    name = 'c3.ai'
+
+    def getJobs(self, driver, web_page, company='c3.ai') -> []:
+        print(f'[{self.name}] Scrap page: {web_page}')
+        driver.implicitly_wait(5)
+        driver.get(web_page)
+        button = driver.find_element(By.XPATH, '//div[@id="jobBtnHolder"]/button')
+        driver.execute_script("arguments[0].click();", button)
+        result = to_records(driver, company)
+        print(f'[{self.name}] Scraped {len(result)} jobs from {web_page}')
+        return result
---
+++ test/test_c3_scraper.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.scrape_c3 import ScrapeC3
+from selenium import webdriver
+
+# python test/test_c3_scraper.py
+options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+url1 = "https://c3.ai/careers"
+jobs = ScrapeC3().getJobs(driver, url1)
+for job in jobs:
+    print(job)
+
+driver.close()
