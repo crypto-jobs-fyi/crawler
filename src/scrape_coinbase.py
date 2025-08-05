2f3ab4882f2fba48dab022ea368264aefb7fbccb
Yury Dubinin <yury.dubinin@gmail.com>
2025-08-05 15:23:06 +0200

    fix: correct company name casing and improve wait times in Coinbase scraper

--- src/scrape_coinbase.py
+++ src/scrape_coinbase.py
@@ -5,23 +5,23 @@ from src.scrape_it import ScrapeIt
 
 # https://www.coinbase.com/careers/positions
 class ScrapeCoinbase(ScrapeIt):
-    name = 'coinbase'
+    name = 'Coinbase'
 
     def getJobs(self, driver, web_page, company='coinbase') -> list:
         print(f'[{self.name}] Scrap page: {web_page}')
         driver.get(web_page)
-        driver.implicitly_wait(7)
+        driver.implicitly_wait(9)
         time.sleep(5)
         # open all departments
         acceptAll = driver.find_elements(By.XPATH, '//span[.="Accept all"]')
         if len(acceptAll) > 0:
             acceptAll[0].click()
-            time.sleep(1)
+            time.sleep(3)
         departments = driver.find_elements(By.XPATH, '//div[@data-testid="positions-department"]')
         print(f'[{self.name}] Found {len(departments)} departments.')
         for department in departments:
             department.click()
-            time.sleep(1)
+            time.sleep(3)
         group_elements = driver.find_elements(By.XPATH, '//div/a[contains(@href, "careers/positions")]')
         result = []
         for elem in group_elements:
