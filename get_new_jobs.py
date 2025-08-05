7f5b17c02af2676a318fcfb3a3a3c116d7e0fc59
Yury Dubinin <yury.dubinin@gmail.com>
2025-08-05 16:03:38 +0200

    fix: simplify job link output in get_new_ai_jobs.py and get_new_jobs.py

--- get_new_jobs.py
+++ get_new_jobs.py
@@ -20,7 +20,7 @@ for job in jobs:
 
 print(f"Found {len(recent_jobs)} new jobs.")
 for job in recent_jobs:
-    print(f"{job['title']} @ {job['company'].capitalize()} -> {job['link'].split('href=')[1].split('target=')[0]}")
+    print(f"{job['title']} @ {job['company'].capitalize()} -> {job['link']}")
 
 with open(jobs_new_file, 'w') as file:
     rj = {'data': recent_jobs}
