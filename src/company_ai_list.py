2b46e19533521fdd2e04ab0f1f924581b64c7697
Yury Dubinin <yury.dubinin@gmail.com>
2025-09-01 12:13:01 +0200

    Add snorkelai to the company list in get_company_list function

--- src/company_ai_list.py
+++ src/company_ai_list.py
@@ -74,4 +74,5 @@ def get_company_list() -> list[CompanyItem]:
         CompanyItem('exa.ai', 'https://jobs.ashbyhq.com/exa', Scrapers.ASHBYHQ.value, 'https://exa.ai'),
         CompanyItem('jua.ai', 'https://job-boards.eu.greenhouse.io/juaai', Scrapers.GREENHOUSE.value, 'https://jua.ai'),
         CompanyItem('c3.ai', 'https://c3.ai/careers', Scrapers.C3.value, 'https://c3.ai'),
+        CompanyItem('snorkelai', 'https://job-boards.greenhouse.io/snorkelai', Scrapers.GREENHOUSE.value, 'https://snorkel.ai'),
     ]
