8e4fdf83efbc7448c373ea068833e38e7aeebfc4
Yury Dubinin <yury.dubinin@gmail.com>
2025-09-05 11:17:04 +0200

    Refactor cookie and geo acceptance methods

--- src/scrape_sygnum.py
+++ src/scrape_sygnum.py
@@ -6,19 +6,30 @@ from src.scrape_it import ScrapeIt
 class ScrapeSygnum(ScrapeIt):
     name = 'Sygnum'
 
+    def tryAcceptCookies(self, driver):
+        try:
+            acceptCookies = driver.find_elements(By.XPATH, '//button[.="Essential only"]')
+            if len(acceptCookies) > 0:
+                acceptCookies[0].click()
+        except Exception as e:
+            print(f'[{self.name}] Error accepting cookies: {e}')
+        return
+    
+    def tryAcceptGeo(self, driver):
+        try:
+            acceptGeo = driver.find_elements(By.XPATH, '//a[.="CONTINUE"]')
+            if len(acceptGeo) > 0:
+                acceptGeo[0].click()
+        except Exception as e:
+            print(f'[{self.name}] Error accepting geo: {e}')
+        return
+
     def getJobs(self, driver, web_page, company = 'sygnum') -> list:
         print(f'[{self.name}] Scrap page: {web_page}')
-        driver.implicitly_wait(7)
+        driver.implicitly_wait(5)
         driver.get(web_page)
-        acceptGeo = driver.find_elements(By.XPATH, '//a[.="CONTINUE"]')
-        if len(acceptGeo) > 0:
-            acceptGeo[0].click()
-            time.sleep(2)
-        # accept cookies
-        acceptCookies = driver.find_elements(By.XPATH, '//button[.="Essential only"]')
-        if len(acceptCookies) > 0:
-            acceptCookies[0].click()
-            time.sleep(3)
+        self.tryAcceptCookies(driver)
+        self.tryAcceptGeo(driver)
         # use reverse strategy from a link to a title
         group_elements = driver.find_elements(By.XPATH, '//ul/li[@class="results-list__item result-item"]')
         result = []
