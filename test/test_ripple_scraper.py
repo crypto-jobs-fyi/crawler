33a377e5ba03e054c95cb8fc90b860a04c082235
yurydubinin <62520712+yury-dubinin@users.noreply.github.com>
2025-11-09 10:56:55 +0100

    fix: adjust import statements in test_ripple_scraper.py for proper module access

--- test/test_ripple_scraper.py
+++ test/test_ripple_scraper.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from selenium import webdriver
 from src.scrape_ripple import ScrapeRipple
 
