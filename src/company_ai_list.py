2e4e8455f00fb1623d9f218899ed974367b87c63
Yury Dubinin <yury.dubinin@gmail.com>
2025-12-15 21:53:22 +0100

    chore: add inworld-ai to company list

--- src/company_ai_list.py
+++ src/company_ai_list.py
@@ -99,4 +99,5 @@ def get_company_list() -> list[CompanyItem]:
         CompanyItem('anrok.com', 'https://jobs.ashbyhq.com/anrok', Scrapers.ASHBYHQ.value, 'https://anrok.com'),
         CompanyItem('harmattan-ai', 'https://jobs.ashbyhq.com/harmattan-ai', Scrapers.ASHBYHQ.value, 'https://harmattan.ai'),
         CompanyItem('radai', 'https://jobs.ashbyhq.com/radai', Scrapers.ASHBYHQ.value, 'https://www.radai.com'),
+        CompanyItem('inworld-ai', 'https://jobs.ashbyhq.com/inworld-ai', Scrapers.ASHBYHQ.value, 'https://www.inworld.ai'),
     ]
