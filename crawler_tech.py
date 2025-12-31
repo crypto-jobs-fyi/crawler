6cffd2a5983812754f5b7b448891135c12f6b43d
Yury Dubinin <yury.dubinin@gmail.com>
2025-12-31 09:56:50 +0100

    Added tech crawler feature

---
+++ crawler_tech.py
@@ -0,0 +1,56 @@
+import time
+from datetime import datetime
+
+from selenium import webdriver
+
+from src.scrapers import Scrapers
+from src.company_item import CompanyItem
+from src.scrape_it import ScrapeIt
+from src.company_tech_list import get_company_list
+from src.companies import Companies
+
+
+jobs_file = 'tech_jobs.json'
+current_jobs_file = 'tech_current_jobs.json'
+companies_file = 'tech_companies.json'
+
+company_list: list[CompanyItem] = get_company_list()
+print(f'[CRAWLER] Number of companies: {len(company_list)}')
+Companies.write_companies(companies_file, company_list)
+
+with open(jobs_file, 'w') as f:
+    f.write('{}')
+
+with open(current_jobs_file, 'w') as cf:
+    cf.write('{}')
+
+# setup headless webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-extensions')
+driver = webdriver.Chrome(options=chrome_options)
+
+# No exclusion as requested
+filtered_companies = company_list
+
+n = 1
+now = datetime.date(datetime.now())
+start_time = time.time()
+for company in filtered_companies:
+    st = time.time()
+    print(f'[CRAWLER] scrape {n} of {len(company_list)}')
+    n = n + 1
+    try:
+        crawler_type: ScrapeIt = company.scraper_type()
+        jobs_data = crawler_type.getJobs(driver, company.jobs_url, company.company_name)
+        ScrapeIt.write_jobs(jobs_data, jobs_file)
+        ScrapeIt.write_current_jobs_number(company.company_name, len(jobs_data), current_jobs_file)
+        print(f'[CRAWLER] Company {company.company_name} has {len(jobs_data)} open positions on {now}')
+        print('[CRAWLER] Execution time:', round(time.time() - st), 'seconds')
+    except Exception as e:
+        print(e)
+        print(f'Company {company.company_name} failed to process...')
+print('[CRAWLER] Execution time:', round((time.time() - start_time)/60), 'minutes')
+driver.close()
--- src/company_tech_list.py
+++ src/company_tech_list.py
@@ -5,7 +5,7 @@ from src.scrapers import Scrapers
 def get_company_list() -> list[CompanyItem]:
     return [
         CompanyItem('life360', 'https://job-boards.greenhouse.io/life360', Scrapers.GREENHOUSE.value, 'https://www.life360.com'),
-        CompanyItem("kong", "https://jobs.lever.co/kong", Scrapers.LEVER.value, "https://www.konghq.com"),
+        CompanyItem("kong", "https://jobs.ashbyhq.com/kong", Scrapers.ASHBYHQ.value, "https://www.konghq.com"),
         CompanyItem("snaplogic", "https://jobs.lever.co/snaplogic", Scrapers.LEVER.value, "https://www.snaplogic.com"),
         CompanyItem('runpod', 'https://job-boards.greenhouse.io/runpod', Scrapers.GREENHOUSE.value, 'https://runpod.io'),
         CompanyItem('tenable', 'https://job-boards.greenhouse.io/tenableinc', Scrapers.GREENHOUSE.value, 'https://www.tenable.com'),
