fdacf9a5b996bab872e43912a8e08cf079d32ffe
Yury Dubinin <yury.dubinin@gmail.com>
2026-01-04 12:18:25 +0100

    fix: update jobs_age_file to use crypto_jobs_age.json

--- get_new_jobs.py
+++ get_new_jobs.py
@@ -1,7 +1,7 @@
 from src.job_processor import get_new_jobs
 
 jobs_file = 'jobs.json'
-jobs_age_file = 'jobs_age.json'
+jobs_age_file = 'crypto_jobs_age.json'
 jobs_new_file = 'jobs_new.json'
 job_age_number = 3  # Number of days to consider a job as new
 
