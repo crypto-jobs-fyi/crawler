53b4b9d1b778888a7a686275f53f6a689bd3059b
Yury Dubinin <yury.dubinin@gmail.com>
2026-01-13 18:34:43 +0100

    Refactor code structure for improved readability and maintainability

--- get_new_jobs.py
+++ get_new_jobs.py
@@ -1,8 +1,8 @@
 from src.job_processor import get_new_jobs
 
-jobs_file = 'jobs.json'
+jobs_file = 'crypto_jobs.json'
 jobs_age_file = 'crypto_jobs_age.json'
-jobs_new_file = 'jobs_new.json'
+jobs_new_file = 'crypto_jobs_new.json'
 job_age_number = 3  # Number of days to consider a job as new
 
 get_new_jobs(jobs_file, jobs_age_file, jobs_new_file, job_age_number)
