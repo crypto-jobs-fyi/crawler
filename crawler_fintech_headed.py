a5c22510d8c178a7c6cf36ce932002839dd863e2
Yury Dubinin <yury.dubinin@gmail.com>
2026-02-05 11:26:26 +0100

    up Cleo and Huspy

--- crawler_fintech_headed.py
+++ crawler_fintech_headed.py
@@ -7,7 +7,7 @@ from src.crawler_runner import CrawlerRunner
 jobs_file = 'headed_fintech_jobs.json'
 current_jobs_file = 'headed_fintech_current_jobs.json'
 
-filtered_companies = Companies.filter_companies_by_name(category="fintech", company_names=['huspy'])
+filtered_companies = Companies.filter_companies_by_name(category="fintech", company_names=['huspy', 'Cleo'])
 
 runner = CrawlerRunner(jobs_file, current_jobs_file, headless=False)
 runner.run(filtered_companies)
