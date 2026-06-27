9ffc4bb8314ee1bf4183fd3bfdebccbc6ef754d6
Yury Dubinin <yury.dubinin@gmail.com>
2026-06-27 10:59:40 +0200

    up helsing.ai

--- crawler_ai_headed.py
+++ crawler_ai_headed.py
@@ -5,7 +5,7 @@ from src.crawler_runner import CrawlerRunner
 jobs_file = 'headed_ai_jobs.json'
 current_jobs_file = 'headed_ai_current_jobs.json'
 
-filtered_companies = Companies.filter_companies_by_name(category="ai", company_names=['Cleo'])
+filtered_companies = Companies.filter_companies_by_name(category="ai", company_names=['helsing.ai'])
 
 runner = CrawlerRunner(jobs_file, current_jobs_file, headless=False)
 runner.run(filtered_companies)
