d68f15f5c66a963e488bc73a0e3b3649cfdcd207
Yury Dubinin <yury.dubinin@gmail.com>
2026-09-15 15:09:05 +0200

    Update Cleo job listings and remove Cleo from fintech filter

--- crawler_fintech_headed.py
+++ crawler_fintech_headed.py
@@ -5,7 +5,7 @@ from src.crawler_runner import CrawlerRunner
 jobs_file = 'headed_fintech_jobs.json'
 current_jobs_file = 'headed_fintech_current_jobs.json'
 
-filtered_companies = Companies.filter_companies_by_name(category="fintech", company_names=['huspy', 'Cleo'])
+filtered_companies = Companies.filter_companies_by_name(category="fintech", company_names=['huspy'])
 
 runner = CrawlerRunner(jobs_file, current_jobs_file, headless=False)
 runner.run(filtered_companies)
